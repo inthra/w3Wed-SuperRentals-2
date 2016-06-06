5f5d6d451f081585d43a00241a690aa4b768ac99
Inthrayuth Mahaphol and Nick Lyman <nick@>
2016-06-06 11:19:22 -0700

    Fix syntax errors in rental.js model to use attributes.

--- app/models/rental.js
+++ app/models/rental.js
@@ -1,9 +1,10 @@
 import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
 
 export default Model.extend({
-  owner: .attr(),
-  city: .attr(),
-  type: .attr(),
-  image: .attr(),
-  bedrooms: .attr()
+  owner: attr(),
+  city: attr(),
+  type: attr(),
+  image: attr(),
+  bedrooms: attr()
 });
