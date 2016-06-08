2037b364f0741b5ebcc7bec0c0f2e0b2489a2fbc
Inthrayuth Mahaphol and Nick Lyman <nick@>
2016-06-08 16:11:30 -0700

    Updated city and rental models for one-to-many relationships (one city to many rentals). Imported (import {hasMany, belongsTo} from ember-data/relationships;) in order to fix errors that said we were missing modules. Still have errors for displaying city name using (rental.city.name).

--- app/models/city.js
+++ app/models/city.js
@@ -1,7 +1,9 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import {hasMany, belongsTo} from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr(),
-  country: attr()
+  country: attr(),
+  rentals: hasMany('rental', { async: true})
 });
--- app/models/rental.js
+++ app/models/rental.js
@@ -1,9 +1,10 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import {hasMany, belongsTo} from 'ember-data/relationships';
 
 export default Model.extend({
   owner: attr(),
-  city: attr(),
+  city: belongsTo('city', {async: true}),
   type: attr(),
   image: attr(),
   bedrooms: attr()
