fa40c34ef9b611f28fea408ec64674dd2a069682
Inthrayuth Mahaphol and Nick Lyman <nick@>
2016-06-08 09:18:52 -0700

    Add cities model, update JSON in Firebase, list cities on index.hbs.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash({
+      cities: this.store.findAll('city'),
+      rentals: this.store.findAll('rental')
+    });
   }, //Comma is for listing more than just the model, doesn't break anything by having it in Ember, might cause problems in Angular
 
   actions: {
